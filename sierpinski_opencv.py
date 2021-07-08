2a512aedc1153aef376e9d8457021399f93c2cf3
Oliver Lomax <oliver.lomax@abingdon.org.uk>
2021-07-08 15:54:17 +0100

    using open cv

---
+++ sierpinski_opencv.py
@@ -0,0 +1,34 @@
+import numpy as np
+import cv2
+
+import random
+
+class Sierpinski:
+    def __init__(self, side_length) -> None:
+        # Triangle
+        self.width = side_length
+        self.height = int(side_length*3**0.5//2)
+        self.corners = [(0, self.height), (self.width, self.height), (self.width/2, 0)]
+        
+        self.current_point = random.choice(self.corners)
+        self.grid = np.zeros(shape=(self.width+1,self.height+1, 3))
+
+    def calc_next_point(self):
+        random_corner = random.choice(self.corners)
+        new_point = int((random_corner[0] + self.current_point[0]) // 2) , int((random_corner[1] + self.current_point[1]) // 2)
+        self.grid[new_point] = (255, 255, 255)
+        self.current_point = new_point
+        print(self.grid)
+
+    def get_image(self):
+        return self.image
+
+sier = Sierpinski(1000)
+
+while True:    
+    # Get a numpy array to display from the simulation
+    npimage=sier.grid
+    sier.calc_next_point()
+    cv2.imshow('image',npimage)
+    if cv2.waitKey(1) == ord('q'):
+        break
